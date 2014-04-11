aea16edec822122271cc23f1840500852e489e7d
Sean Freitag <freitag.sean@gmail.com>
2014-04-11 13:33:13 -0500

    Adding in a concept of a data provider.

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -1,17 +1,17 @@
-# Event schema
+# DataProvider schema
 
 # --- !Ups
 
-CREATE TABLE Event (
-    id       BIGSERIAL                PRIMARY KEY,
-    date     TIMESTAMP WITH TIME ZONE NOT NULL,
-    type     TEXT                     NOT NULL,
-    payload  BYTEA                    NOT NULL
+CREATE TABLE DataProvider (
+    id       BIGSERIAL PRIMARY KEY,
+    name     TEXT      NOT NULL,
+    token    TEXT      NOT NULL
 );
 
-CREATE INDEX Event_date ON Event(date);
-CREATE INDEX Event_type ON Event(type);
+CREATE INDEX DataProvider_name ON DataProvider(name);
+
+INSERT INTO DataProvider (name, token) VALUES ('Sean Freitag', 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC4wIB4s3oODQ97rzMEdADDsbVxUvmsUSmkBy00ht/SdEzFfI4YBb5v+uN7OecdU4QUrMEce1cjku9X9WrImNJrcOrFqDlhveWOTQhf66jQbcJtfQ90ohRV44yFUfulgMg+f5uPvynf2zlFLzrZqD5jovBsK54XXUayVxkkk96Vhu6STt5EXtTsRaRQ0WVaWs5FsI8ivOA2PlGdbnBPNaVa/2Slf0CP9UnuprDjmYdGbHCbaweKYXi2R+n3KMAEVqagb1qtPCFo7nDBF1rLZEu0N39Oj7Q6Ed3iFiETkNB2cmfwrCAadvJRfJL5ElfZSlw5EDYfGH1KGj6lXa5midBr');
 
 # --- !Downs
 
-DROP TABLE Event;
+DROP TABLE DataProvider;
---
+++ conf/evolutions/default/3.sql
@@ -0,0 +1,18 @@
+# Event schema
+
+# --- !Ups
+
+CREATE TABLE Event (
+    id           BIGSERIAL                PRIMARY KEY,
+    provider_id  BIGINT                   REFERENCES DataProvider(id),
+    date         TIMESTAMP WITH TIME ZONE NOT NULL,
+    type         TEXT                     NOT NULL,
+    payload      BYTEA                    NOT NULL
+);
+
+CREATE INDEX Event_date ON Event(date);
+CREATE INDEX Event_type ON Event(type);
+
+# --- !Downs
+
+DROP TABLE Event;
