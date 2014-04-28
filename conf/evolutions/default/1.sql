fcc3784ba932521c997e952dbe7dbf36422a2408
Sean Freitag <freitag.sean@gmail.com>
2014-04-27 23:22:38 -0500

    Adding in CRUD operations for SiteData.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -5,13 +5,12 @@
 CREATE TABLE SiteData (
     id       BIGSERIAL PRIMARY KEY,
     sitename TEXT      NOT NULL,
-    mockdata BOOLEAN   NOT NULL,
-    date     TIMESTAMP NOT NULL
+    date     BIGINT    NOT NULL,
+    data     TEXT      NOT NULL
 );
 
 CREATE INDEX SiteData_sitename ON SiteData(sitename);
 CREATE INDEX SiteData_date ON SiteData(date);
-CREATE INDEX SiteData_mockdata ON SiteData(mockdata);
 
 # --- !Downs
 
