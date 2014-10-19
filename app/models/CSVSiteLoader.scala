1b57050471eb4d268e40ca470eb3d54c558a519e
Sean Freitag <sean@gneoxsolutions.com>
2014-10-19 15:11:02 -0500

    Adding in the remaining pieces of data for the Trillium site.

--- app/models/CSVSiteLoader.scala
+++ app/models/CSVSiteLoader.scala
@@ -23,7 +23,16 @@ case class TrilliumSiteLoader(file: String) extends CSVSiteLoader {
       calendar.getTimeInMillis,
       "trillium",
       SiteGHXData(split(3).toDouble, split(5).toDouble, split(4).toDouble),
-      Seq()
+      Seq(
+        SiteNodeData(split(6).toDouble, split(8).toDouble, split(7).toDouble), //CP1
+        SiteNodeData(split(9).toDouble, split(11).toDouble, split(10).toDouble), //BTU1
+        SiteNodeData(split(12).toDouble, split(14).toDouble, split(11).toDouble), //BTU2
+        SiteNodeData(split(15).toDouble, split(25).toDouble, split(16).toDouble), //BTU3
+        SiteNodeData(split(26).toDouble, split(36).toDouble, split(27).toDouble), //BTU4
+        SiteNodeData(split(37).toDouble, split(39).toDouble, split(38).toDouble), //HEX-1
+        SiteNodeData(split(41).toDouble, split(43).toDouble, split(40).toDouble), //P-HP4
+        SiteNodeData(split(44).toDouble, split(46).toDouble, split(45).toDouble) //Hybrid Switch
+      )
     )
   }
 }
